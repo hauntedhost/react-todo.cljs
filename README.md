f5e5ccc9e7275e758d5a1d94f020e364ad379432
Sean Omlor <somlor@eml.cc>
2015-04-26 22:51:27 -0400

    initial

---
+++ README.md
@@ -0,0 +1,15 @@
+### react-todo
+
+Tiny toy todo app experiment with Clojurescript / React.js.
+
+Thanks to @bhauman for guidance.
+
+### Getting started:
+```bash
+$ rlwrap lein figwheel
+$ open http://localhost:3449
+```
+
+#### License
+
+Released under the MIT license.
---
+++ src/react_todo/core.cljs
@@ -0,0 +1,70 @@
+(ns ^:figwheel-always react-todo.core
+    (:require [sablono.core :as sab :include-macros true]))
+
+(enable-console-print!)
+
+(println "welcome! 吃饭了吗?")
+
+;; define your app data so that it doesn't get over-written on reload
+(defonce app-state (atom {:items ["practice 普通话"
+                                  "inbox zero"]}))
+
+(defn add-todo [todo]
+  (swap! app-state dissoc :form-field)
+  (swap! app-state (fn [data]
+    (update-in data [:items] (fn [items]
+      (conj items todo))))))
+
+(defn delete-todo [index]
+  (swap! app-state
+    (fn [data] (update-in data [:items]
+      (fn [items]
+        (reduce-kv (fn [a k v]
+          (if (= k index) a (vec (conj a v)))) [] items))))))
+
+(defn input-change-handler [event]
+  (.preventDefault event)
+  (let [value (.. event -target -value)]
+    (swap! app-state
+      assoc-in [:form-field] value)))
+
+(defn submit-todo-handler [event]
+  (.preventDefault event)
+  (let [new-todo (:form-field @app-state)]
+    (add-todo new-todo)))
+
+(defn delete-todo-handler [index event]
+  (.preventDefault event)
+  (delete-todo index))
+
+(defn todo-item [index todo]
+  [:li {:key (str "todo_" index)} todo " "
+    [:a {:key (str index)
+         :href "#"
+         :onClick (partial delete-todo-handler index)} "delete"]])
+
+(defn todo-list [items]
+  (sab/html [:ul
+            (map-indexed todo-item items)]))
+
+(defn todo-wrapper [data]
+  (sab/html [:div
+              [:h1 "Todos"]
+              [:form {:onSubmit submit-todo-handler}
+                [:input {:name "todo"
+                         :value (:form-field data)
+                         :onChange input-change-handler}]]
+              (todo-list (:items data))]))
+
+(defn render [data]
+  (.render js/React
+           (todo-wrapper data)
+           (.getElementById js/document "app")))
+
+(defonce adding-app-watch
+  (add-watch app-state :on-change
+    (fn [_ _ _ new-state]
+     (prn new-state)
+     (render new-state))))
+
+(render @app-state)
